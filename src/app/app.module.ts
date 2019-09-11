0f7d0cd2a6b5d040a742e11d6f1e24f0f1386c4b
Tony <thetonyho@gmail.com>
2019-09-11 17:23:01 +1000

    Single Page Angular to JupyterNotebooks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// Socket-io
+// REFERENCE: https://alligator.io/angular/socket-io/
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+const config: SocketIoConfig = { url: 'ws://jupyteralb-1393537276.ap-southeast-2.elb.amazonaws.com', options: {} };
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
